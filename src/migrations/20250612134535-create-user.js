f6722957e24abeb5b3e61678a2697aa500ede120
adrianolopees <adrianodeev1@gmail.com>
2025-06-12 11:07:11 -0300

    Implement code changes to enhance functionality and improve performance

---
+++ src/migrations/20250612134535-create-user.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING(50),
+        allowNull: false,
+        unique: true
+      },
+      email: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        unique: true
+      },
+      password_hash: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      created_at: {
+        allowNull: true,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
